9749827e068de07aa4bfdaaf53dd9b39f9a5a48d
Ephraim Kunz <ephraimkunz@me.com>
2016-12-03 20:45:54 -0700

    Add basic sentiment analysis and Hero cards

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var restify = require('restify');
 var builder = require('botbuilder');
 var prettydate = require('pretty-date');
 var twitterClient = require('twitter-node-client').Twitter;
+var sentiment = require('sentiment');
+
+var twitterAuth = {
+	"consumerKey":"gLwyLjFl7nxKxfrlqqcZfm7b8",
+	"consumerSecret": "637BefF7DMZZo3tXf1eqgVeJLMeLj9fV76589fKZv07VBqO7Yw",
+	"accessToken": "3120259401-OWDAF2OXMW9lAia8Fg2Leozuup64GaEPE4RXJNZ",
+	"accessTokenSecret": "lO165m4gW6jFkJiXSsvScetYn5O0W7aTQSOJhkEkEG1Ji"
+}
 
 //Setup Restify server
 var server = restify.createServer();
@@ -113,6 +121,16 @@ bot.dialog('/nospaces', [
 		session.endDialog(whitesRemoved);
 	}]);
 
+// Text layout of results
+function buildAnalysisResult(analysis){
+	var result = '';
+	result += '\tTotal score = ' + analysis.score + '\t';
+	result += '\tComparative score = ' + analysis.comparative;
+	// result += '\tSome positive words = ' + analysis.positive.slice(0, 5) + '\n';
+	// result += '\tSome negative words = ' + analysis.negative.slice(0, 5) + '\n';
+	return result;
+}
+
 bot.dialog('/sentiment-analysis', [
 	function(session){
 		builder.Prompts.text(session, "Enter tweet search (prepend hashtag searches with #)");
@@ -121,13 +139,41 @@ bot.dialog('/sentiment-analysis', [
 		session.send("Crunching the data...");
 		session.sendTyping();
 	 	var error = function (err, response, body) {
-        	console.log('ERROR [%s]', err);
         	session.endDialog(err.toString() + response.toString());
     	};
+
 	    var success = function (data) {
-	        console.log('Data [%s]', data);
-	        session.endDialog(data.toString());
-	    };
-	    var twitter = new twitterClient();
-	    twitter.getSearch({'q':'#haiku','count': 10}, error, success);
+	        data = JSON.parse(data);
+	        var tweetText = '';
+	        data.statuses.forEach(function(tweet){
+	        	tweetText += (' ' + tweet.text); //Space so we don't clobber words on the fault line
+	        });
+	        console.log(tweetText);
+
+	        var analysis = sentiment(tweetText);
+
+	        var card = new builder.HeroCard(session)
+            .title("Analysis results")
+            .subtitle(data.statuses.length + ' tweets analyzed')
+            .text(buildAnalysisResult(analysis));
+            if(analysis.score > 0){
+            	card.images([
+                 	builder.CardImage.create(session, "https://s-media-cache-ak0.pinimg.com/originals/97/72/6f/97726fe8f02ba122e9436eb80a28fb2a.png")
+            	]);	
+            }
+            else if(analysis.score < 0){
+            	card.images([
+                 	builder.CardImage.create(session, "https://s-media-cache-ak0.pinimg.com/736x/f5/8b/ec/f58bece565008387c95ee25036b84ad3.jpg")
+            	]);	
+            }
+            else{
+	            card.images([
+	                 builder.CardImage.create(session, "http://cosmouk.cdnds.net/15/35/1440495683-wpid-wp-1436657947930.jpeg")
+	            ]);
+        	}
+	        var msg = new builder.Message(session).attachments([card]);
+	        session.endDialog(msg);
+		};
+	    var twitter = new twitterClient(twitterAuth);
+	    twitter.getSearch({'q':results.response,'count': 100, 'lang': 'en'}, error, success);
 	}]);
